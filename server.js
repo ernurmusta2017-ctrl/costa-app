f101a8632e02a699c5999c6e0300d0d8101407d6
ernurmusta2017-ctrl <ernurmusta2017@gmail.com>
2026-09-02 20:49:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,10 @@ app.use(express.static(__dirname));
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }
+    ssl: { rejectUnauthorized: false },
+    max: 10,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
 });
 
 // --- Root Health Check (Required for frontend DB Control Center handshake) ---
